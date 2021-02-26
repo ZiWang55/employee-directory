db3d0945b743fc61ace7a76a8f308af9b5f8681d
zi wang <ziwang55@gmail.com>
2021-02-25 22:57:34 -0600

    components

---
+++ src/components/Directory.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import Employees from "./Employees.js";
+import Search from "./Search.js";
+import API from "../utils/API";
+import "../styles/Directory.css";
+
--- src/components/Employees.js
+++ src/components/Employees.js
@@ -4,6 +4,18 @@ const Employees = (props) => {
     // Formate date to make it readable
     function formateDate(date) {
         const dateArray = date.split("-");
+        const year = dateArray[0];
+        const month = dateArray[1];
+        const dayArray = dateArray[2].split("T");
+        const day = dayArray[0];
+        const formattedDate = [month, day, year].join("-");
+        return formattedDate;
         
     }
+
+    // Cleanly call formatDate on the props in the below JSX
+    // let dateOfBirth = formateDate(props.dob);
+
+    // return
+
 }
