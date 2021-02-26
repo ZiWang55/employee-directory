631b66b6e15af14f4df8a07f5a4dee274a9a0c1c
zi wang <ziwang55@gmail.com>
2021-02-25 23:15:03 -0600

    Employees.js

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Employees = (props) => {
     // Formate date to make it readable
-    function formateDate(date) {
+    function formatDate(date) {
         const dateArray = date.split("-");
         const year = dateArray[0];
         const month = dateArray[1];
@@ -13,17 +13,23 @@ const Employees = (props) => {
         
     }
 
-    // Cleanly call formatDate on the props in the below JSX
-    // let dateOfBirth = formateDate(props.dob);
+  // dateOfBirth variable allows me to cleanly call formatDate on the props in the below JSX
+  let dateOfBirth = formatDate(props.dob);
 
-    // return (
-    //     <tr className="tr">
-    //         <td>
-    //             <img alt=[props.firstName] SRC={props.icon}/>
-    //         </td>
-    //     </tr>
-    // )
-
+  return (
+    <tr className="tr">
+      <td>
+        <img alt={props.firstName} src={props.icon} />
+      </td>
+      <td>
+        {props.firstName} {props.lastName}
+      </td>
+      <td>{props.email}</td>
+      <td>{props.phone} </td>
+      <td>{dateOfBirth}</td>
+      <td>{props.address}</td>
+    </tr>
+  );
 }
 
 export default Employees
